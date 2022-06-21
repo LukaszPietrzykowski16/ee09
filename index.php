faf508abbb3d80c29ca3e75dc8f99fde4056b9a2
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-06-21 10:20:43 +0200

    php

--- index.php
+++ index.php
@@ -5,9 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Piłka nożna</title>
 </head>
 <body>
-    
+    <header><h3>Reprezentacja polski</h3>
+    <img src="obraz1.jpg" alt="reprezentacja">
+    <header>
+    <div id="lewy">
+        <form method="POST">
+            <select> 
+                <option value="bramkarz"> bramkarz </option> 
+                <option value="pomocnik"> pomocnik </option> 
+                <option value="obronca"> obrońca </option> 
+                <option value="napastnik"> napastnik </option> 
+            </select>
+            <input type="submit"/>
+        </form>
+    </div> 
+    <div id="prawy">
+    </div>
+    <div id="glowny">
+    </div>
+    <div id="liga">
+    </div>
 </body>
 </html>
